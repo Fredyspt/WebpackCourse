0bfd32b5dd2248f357dd53982658cc460b639725
Fredyspt <fredyfps@gmail.com>
2021-07-07 14:52:50 -0600

    Added configurations for HTML and CSS as well as preprocessors
    Signed-off-by: Fredyspt <fredyfps@gmail.com>

--- portfolio/webpack.config.js
+++ portfolio/webpack.config.js
@@ -1,4 +1,7 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
   entry: "./src/index.js",
@@ -18,6 +21,21 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, 
+        'css-loader', 
+        'sass-loader'
+        ],
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true, 
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin(),
+  ]
 };
