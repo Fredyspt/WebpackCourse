9ae679d75ce0c80b0ecfd1ea00952bc765ec73af
Fredyspt <fredyfps@gmail.com>
2021-07-07 18:53:53 -0600

    Added optimizations to import fonts and images
    Signed-off-by: Fredyspt <fredyfps@gmail.com>

--- portfolio/webpack.config.js
+++ portfolio/webpack.config.js
@@ -6,10 +6,14 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: "./src/index.js",
   output: {
+    // path.resolve gives the absolute path of project in the OS, and 
+    // outputs everything on the dist folder
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   resolve: {
+    // files that webpack will read
     extensions: [".js"],
   },
   module: {
@@ -18,7 +22,7 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
@@ -28,6 +32,28 @@ module.exports = {
         'sass-loader'
         ],
       },
+      {
+        test: /\.png/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            // Enables or disables file transformation of base64
+            limit: 10000,
+            // (Multipurpose Internet Mail Extensions)
+            // Standard way of sending content through the network
+            mimetype: "application/font-woff",
+            // Initial name + extension
+            name: "[name].[ext]", 
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false
+          }
+        }
+      }
     ],
   },
   plugins: [
