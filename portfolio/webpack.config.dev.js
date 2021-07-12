9190f8eff94860a03522bf5ea228b8b4d369f611
Fredyspt <fredyfps@gmail.com>
2021-07-11 19:06:44 -0600

    Added webpack dev server and bundle analyzer
    Signed-off-by: Fredyspt <fredyfps@gmail.com>

--- portfolio/webpack.config.dev.js
+++ portfolio/webpack.config.dev.js
@@ -1,30 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    // path.resolve gives the absolute path of project in the OS, and 
+    // path.resolve gives the absolute path of project in the OS, and
     // outputs everything on the dist folder
     path: path.resolve(__dirname, "dist"),
-    // with [contenthash] we obtain a hash per build, which changes everytime 
+    // with [contenthash] we obtain a hash per build, which changes everytime
     // changes are made to the project
     filename: "[name].[contenthash].js",
-    assetModuleFilename: 'assets/images/[hash][ext][query]'
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
-  mode: 'development',
+  mode: "development",
   resolve: {
     // files that webpack will read
     extensions: [".js"],
     alias: {
-      '@utils': path.resolve(__dirname, 'src/utils/'),
-      '@templates': path.resolve(__dirname, 'src/templates/'),
-      '@styles': path.resolve(__dirname, 'src/styles/'),
-      '@images': path.resolve(__dirname, 'src/assets/images/')
-    }
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@templates": path.resolve(__dirname, "src/templates/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@images": path.resolve(__dirname, "src/assets/images/"),
+    },
   },
   module: {
     rules: [
@@ -32,24 +33,21 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.s?css$/i,
-        use: [MiniCssExtractPlugin.loader, 
-        'css-loader', 
-        'sass-loader'
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.png/,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2)$/,
         use: {
-          loader: 'url-loader',
+          loader: "url-loader",
           options: {
             // Enables or disables file transformation of base64
             limit: 10000,
@@ -57,33 +55,42 @@ module.exports = {
             // Standard way of sending content through the network
             mimetype: "application/font-woff",
             // Initial name + extension
-            name: "[name].[contenthash].[ext]", 
+            name: "[name].[contenthash].[ext]",
             outputPath: "./assets/fonts/",
             publicPath: "../assets/fonts/",
-            esModule: false
-          }
-        }
-      }
+            esModule: false,
+          },
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      inject: true, 
-      template: './public/index.html',
-      filename: './index.html'
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
     }),
     new MiniCssExtractPlugin({
       // We can set the output directory of the CSS file
-      filename: 'assets/[name].[contenthash].css'
+      filename: "assets/[name].[contenthash].css",
     }),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src", "assets/images"), 
-          to: "assets/images"
-        }
-      ]
-    }), 
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
+    }),
     new Dotenv(),
-  ], 
+    new BundleAnalyzerPlugin(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    historyApiFallback: true,
+    port: 3000,
+    // Opens browser automatically
+    open: true,
+  },
 };
