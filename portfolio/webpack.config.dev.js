4c0ca39bcaca6a1dbb7b0c691211c256d9029858
Fredyspt <fredyfps@gmail.com>
2021-07-12 08:15:11 -0600

    Enabled devtools
    Signed-off-by: Fredyspt <fredyfps@gmail.com>

--- portfolio/webpack.config.dev.js
+++ portfolio/webpack.config.dev.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: "./src/index.js",
@@ -17,6 +17,7 @@ module.exports = {
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   mode: "development",
+  devtool: 'source-map',
   resolve: {
     // files that webpack will read
     extensions: [".js"],
